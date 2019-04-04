a7b386b6053749256c3ef50c2dcaabadce2cfcd7
Brent Rusnell <blrusnell@gmail.com>
2019-04-03 20:17:33 -0400

    changed project to note creator, set table widths

--- app/app.js
+++ app/app.js
@@ -11,6 +11,28 @@ var updateStatusLabel = function(message) {
 	$('#statusLabel').text('Status: ' + message);
 }
 
+
+
+/*
+PAGE CONTENT STUFF
+*/
+
+var createEntry = function(key, value) {
+	return localStorage.setItem(key, value);
+}
+
+var updateEntry = function(key, value) {
+	return localStorage.setItem(key, value);
+}
+
+
+var removeEntry = function(key) {
+	return localStorage.removeItem(key);
+}
+
+/*
+JQUERY STUFF
+*/
  //jQuery document ready initialization stuff
  ////button and form event handlers
  // logic for determining action probably needs to go in the event handler
@@ -72,38 +94,3 @@ $(document).ready(function () {
 	});	
 
 });
-/*
-
-
-
-When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
-
-let form = document.querySelector("form");
-let guestName = form.elements.guest;
-let hatSize = form.elements["hat-size"];
-*/
-
-/*
-PAGE CONTENT STUFF
-*/
-//something to update the table every time localStorage changes
-
-//localStorage stuff
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-////create new entry
-//localStorage.setItem(key, value)
-var createEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
-
-////Update existing entry
-//localStorage.setItem(key, value)
-var updateEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
-
-////delete existing entry
-//localStorage.removeItem(key)
-var removeEntry = function(key) {
-	return localStorage.removeItem(key);
-}
