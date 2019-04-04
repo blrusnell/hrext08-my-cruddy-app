2afac71a82f2b0717403bf40d5bd216b278af625
Brent Rusnell <blrusnell@gmail.com>
2019-04-04 18:29:09 -0400

    can put in image file path, cant display image

--- app/app.js
+++ app/app.js
@@ -32,6 +32,23 @@ var removeEntry = function(key) {
 	return localStorage.removeItem(key);
 }
 
+var convertImgToBase64URL= function(url, callback, outputFormat){
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'), dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}
+
+
 
 
 /*
@@ -44,7 +61,6 @@ $(document).ready(function () {
 	loadLocalStorage();
 
 	updateStatusLabel('There is nothing here!');
-
 
 
 
